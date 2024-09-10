00e7d5b39494974fccff7d14cdc9f037a961f784
Julien Delange <julien.delange@datadoghq.com>
2024-09-09 20:55:30 -0400

    use osv-scanner

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,8 +26,9 @@ fi
 ########################################################
 # osv-scanner
 ########################################################
+echo "Installing osv-scanner"
 mkdir /osv-scanner
-curl -L -o /osv-scanner/osv-scanner.zip https://github.com/DataDog/osv-scanner/releases/download/v0.7.1/osv-scanner_linux_$(uname -m).zip >/dev/null 2>&1 || exit 1
+curl -L -o "/osv-scanner/osv-scanner.zip" "https://github.com/DataDog/osv-scanner/releases/download/v0.7.1/osv-scanner_linux_$(uname -m).zip" >/dev/null 2>&1 || exit 1
 (cd /osv-scanner && unzip osv-scanner.zip)
 chmod 755 /osv-scanner/osv-scanner
 
